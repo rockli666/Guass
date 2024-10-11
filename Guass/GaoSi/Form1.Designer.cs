d3dc0c010dd0d85d5425c36152418365447eb6a5
LSH <168797306+rockli666@users.noreply.github.com>
2024-10-12 01:25:27 +0800

    Add files via upload

---
+++ Guass/GaoSi/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Gauss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.椭球ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cGCS2000ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.正算ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.反算ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.注意ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.椭球ToolStripMenuItem,
+            this.正算ToolStripMenuItem,
+            this.反算ToolStripMenuItem,
+            this.注意ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1390, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开文件ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(82, 38);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 打开文件ToolStripMenuItem
+            // 
+            this.打开文件ToolStripMenuItem.Name = "打开文件ToolStripMenuItem";
+            this.打开文件ToolStripMenuItem.Size = new System.Drawing.Size(243, 44);
+            this.打开文件ToolStripMenuItem.Text = "打开文件";
+            this.打开文件ToolStripMenuItem.Click += new System.EventHandler(this.打开文件ToolStripMenuItem_Click);
+            // 
+            // 椭球ToolStripMenuItem
+            // 
+            this.椭球ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cGCS2000ToolStripMenuItem});
+            this.椭球ToolStripMenuItem.Name = "椭球ToolStripMenuItem";
+            this.椭球ToolStripMenuItem.Size = new System.Drawing.Size(82, 38);
+            this.椭球ToolStripMenuItem.Text = "椭球";
+            // 
+            // cGCS2000ToolStripMenuItem
+            // 
+            this.cGCS2000ToolStripMenuItem.Name = "cGCS2000ToolStripMenuItem";
+            this.cGCS2000ToolStripMenuItem.Size = new System.Drawing.Size(267, 44);
+            this.cGCS2000ToolStripMenuItem.Text = "CGCS2000";
+            this.cGCS2000ToolStripMenuItem.Click += new System.EventHandler(this.cGCS2000ToolStripMenuItem_Click);
+            // 
+            // 正算ToolStripMenuItem
+            // 
+            this.正算ToolStripMenuItem.Name = "正算ToolStripMenuItem";
+            this.正算ToolStripMenuItem.Size = new System.Drawing.Size(82, 38);
+            this.正算ToolStripMenuItem.Text = "正算";
+            this.正算ToolStripMenuItem.Click += new System.EventHandler(this.正算ToolStripMenuItem_Click);
+            // 
+            // 反算ToolStripMenuItem
+            // 
+            this.反算ToolStripMenuItem.Name = "反算ToolStripMenuItem";
+            this.反算ToolStripMenuItem.Size = new System.Drawing.Size(82, 38);
+            this.反算ToolStripMenuItem.Text = "反算";
+            this.反算ToolStripMenuItem.Click += new System.EventHandler(this.反算ToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(13, 43);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1377, 794);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(8, 39);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1361, 747);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "原始数据";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 37;
+            this.dataGridView1.Size = new System.Drawing.Size(1349, 735);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AutoScroll = true;
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(8, 39);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1361, 747);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "计算报告";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(6, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1349, 735);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(8, 39);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1361, 747);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "中间数据";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // 注意ToolStripMenuItem
+            // 
+            this.注意ToolStripMenuItem.Name = "注意ToolStripMenuItem";
+            this.注意ToolStripMenuItem.Size = new System.Drawing.Size(82, 38);
+            this.注意ToolStripMenuItem.Text = "注意";
+            this.注意ToolStripMenuItem.Click += new System.EventHandler(this.注意ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1390, 840);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "高斯正反算";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开文件ToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripMenuItem 椭球ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 正算ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 反算ToolStripMenuItem;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem cGCS2000ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 注意ToolStripMenuItem;
+    }
+}
+
---
+++ Guass/GaoSi/POINT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauss
+{
+    /// <summary>
+    /// 点信息
+    /// </summary>
+    public class POINT
+    {
+        public string Name;
+        public double B, L, H;//大地坐标系
+        public double x, y;//高斯平面直角坐标系
+        public int Zone;//带号
+
+        public override string ToString()
+        {
+            string line = string.Format("{0,-5}",  Name);
+            line += string.Format("{0,15}{1,15}",GeoPro.Rad2Str(B),GeoPro.Rad2Str( L));
+            line += string.Format("{0,15:f3} {1,15:f3}",x,y);
+            return line;
+        }
+    }
+
+    
+}
